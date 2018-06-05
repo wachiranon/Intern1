4a96db1addf61c951da090bb154ff94c6b55acbe
wachiranon <wachiranon_g@cmu.ac.th>
2018-06-05 08:38:19 +0700

    Intern1

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Totxt = new System.Windows.Forms.Button();
+            this.InputLineNum = new System.Windows.Forms.TextBox();
+            this.ShowHello = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // Totxt
+            // 
+            this.Totxt.Location = new System.Drawing.Point(133, 49);
+            this.Totxt.Name = "Totxt";
+            this.Totxt.Size = new System.Drawing.Size(75, 23);
+            this.Totxt.TabIndex = 0;
+            this.Totxt.Text = "button1";
+            this.Totxt.UseVisualStyleBackColor = true;
+            this.Totxt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // InputLineNum
+            // 
+            this.InputLineNum.Location = new System.Drawing.Point(27, 49);
+            this.InputLineNum.Name = "InputLineNum";
+            this.InputLineNum.Size = new System.Drawing.Size(100, 20);
+            this.InputLineNum.TabIndex = 1;
+            // 
+            // ShowHello
+            // 
+            this.ShowHello.Location = new System.Drawing.Point(27, 92);
+            this.ShowHello.Multiline = true;
+            this.ShowHello.Name = "ShowHello";
+            this.ShowHello.Size = new System.Drawing.Size(181, 346);
+            this.ShowHello.TabIndex = 2;
+            this.ShowHello.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(499, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(499, 260);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(606, 92);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(182, 342);
+            this.listBox2.TabIndex = 8;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(268, 92);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(202, 346);
+            this.treeView1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ShowHello);
+            this.Controls.Add(this.InputLineNum);
+            this.Controls.Add(this.Totxt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Totxt;
+        private System.Windows.Forms.TextBox InputLineNum;
+        private System.Windows.Forms.TextBox ShowHello;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //..
+
+            int linetxt = int.Parse(InputLineNum.Text);
+            string txt = "";
+            for(int i=1; i<= linetxt; i++)
+            {
+                string n = Convert.ToString(i);
+                txt = txt + "hello" + n + "\r\n";
+            }
+            Writ_txt(txt);
+
+            string show = File.ReadAllText("Hello.txt");
+            ShowHello.Text = show;
+        }
+        void Writ_txt(string txt)
+        {
+            File.WriteAllText("Hello.txt", txt);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string curDir = Directory.GetCurrentDirectory();
+
+            curDir = "..//..";
+
+            String[] dirs = Directory.GetDirectories(curDir);
+            string[] files = Directory.GetFiles(curDir);
+
+            int j = dirs.Length;
+            for (int i = 0; i<j; i++)
+            {
+                listBox2.Items.Add(dirs[i]);
+            }
+
+            foreach (string file in files)
+            {
+                listBox2.Items.Add(file);
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string curDir = Directory.GetCurrentDirectory();
+            curDir = "..//..";
+            string[] dirs = Directory.GetDirectories(curDir);
+            string[] files = Directory.GetFiles(curDir);
+            int j = dirs.Length;
+            for(int i = 0; i < j; i++)
+            {
+                LoadDirec(dirs[i]);
+            }
+            foreach (string file in files)
+            {
+                treeView1.Nodes.Add(file);
+            }
+
+
+        }
+
+        void LoadDirec(string Dir)
+        {
+            DirectoryInfo dir = new DirectoryInfo(Dir);
+            TreeNode tds = treeView1.Nodes.Add(dir.Name);
+            tds.Tag = dir.FullName;
+            Loadfiles(Dir, tds);
+            LoadsubDirec(Dir,tds);
+        }
+
+        void LoadsubDirec(string Dir,TreeNode td)
+        {
+            string[] subdir = Directory.GetDirectories(Dir);
+            foreach (string subdirectory in subdir)  
+           {  
+  
+               DirectoryInfo di = new DirectoryInfo(subdirectory);  
+               TreeNode tds = td.Nodes.Add(di.Name);  
+               tds.Tag = di.FullName;  
+               Loadfiles(subdirectory, tds);  
+               LoadsubDirec(subdirectory, tds);    
+           }  
+        }
+
+        void Loadfiles(string Dir,TreeNode td)
+        {
+            string[] files = Directory.GetFiles(Dir, "*.*");
+            foreach (string file in files)
+            {
+                FileInfo fi = new FileInfo(file);
+                TreeNode tds = td.Nodes.Add(fi.Name);
+                tds.Tag = fi.FullName;
+            }
+        }
+    }
+}
