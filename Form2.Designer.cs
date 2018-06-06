09472e7e60d8881ec7f44ca740679eaa3206742e
wachiranon <wachiranon_g@cmu.ac.th>
2018-06-06 11:52:02 +0700

    Day2
    Program move_point

---
+++ Form2.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Area = new System.Windows.Forms.Panel();
+            this.PointNum = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Area
+            // 
+            this.Area.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Area.Location = new System.Drawing.Point(32, 73);
+            this.Area.Name = "Area";
+            this.Area.Size = new System.Drawing.Size(688, 365);
+            this.Area.TabIndex = 0;
+            this.Area.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // PointNum
+            // 
+            this.PointNum.Location = new System.Drawing.Point(63, 29);
+            this.PointNum.Name = "PointNum";
+            this.PointNum.Size = new System.Drawing.Size(185, 20);
+            this.PointNum.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(273, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Show Blue";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(375, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(473, 26);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Load";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PointNum);
+            this.Controls.Add(this.Area);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Area;
+        private System.Windows.Forms.TextBox PointNum;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        bool mouse_move = false;
+        int x, y;
+        public Form2()
+        {
+            InitializeComponent();
+            //this.button1.MouseDown += Button1_MouseDown;
+            //this.button1.MouseUp += Button1_MouseUp;
+        }
+
+        private void Button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouse_move = false;
+
+            Control butt = (Control)sender;
+            butt.BackColor = Color.Blue;
+        }
+
+        private void Button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //this.Text = "down";
+            mouse_move = true;
+            Control butt = (Control)sender;
+            //butt.Text = "aaa";
+            //Point p = butt.Location;
+            //butt.Location = new Point(p.X, p.Y);
+            x = e.X;
+            y = e.Y;
+            butt.BackColor = Color.Yellow;
+        }
+
+        private void Button1_Move(object sender, MouseEventArgs e)
+        {
+            //this.Text = "Move";
+            if(mouse_move == true)
+            {
+                Control p = (Control)sender;
+                p.Location = new Point(e.X + p.Left - x, e.Y + p.Top - y);
+            }
+        }
+
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int p_num = int.Parse(PointNum.Text);
+            for(int i = 0; i < p_num; i++)
+            {
+                paint_point(i * 10, i * 10);
+            }
+            point_move(this.Area.Controls);
+
+
+        }
+
+        private void point_move(object panel)
+        {
+            foreach(Control p in this.Area.Controls)
+            {
+                p.MouseUp += Button1_MouseUp;
+                p.MouseDown += Button1_MouseDown;
+                p.MouseMove += Button1_Move;
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Area.Controls.Clear();
+            using (FileStream fs = new FileStream("point.bin", FileMode.Open))
+            {
+                using (BinaryReader r = new BinaryReader(fs))
+                {
+                    while (r.BaseStream.Length != r.BaseStream.Position)
+                    {
+                        int position_x = r.ReadInt32();
+                        int position_y = r.ReadInt32();
+
+                        paint_point(position_x, position_y);
+                    }
+                    fs.Flush();
+                    fs.Close();
+                    point_move(this.Area.Controls);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //..
+
+            //..
+            using (FileStream fs = new FileStream("point.bin", FileMode.Create))
+            {
+                using (BinaryWriter w = new BinaryWriter(fs))
+                {
+                    foreach (Control p in this.Area.Controls)
+                    {
+                        w.Write(p.Left);
+                        w.Write(p.Top);
+                    }
+                    fs.Flush();
+                    fs.Close();
+                }
+            }
+        }
+
+        void paint_point(int x,int y)
+        {
+            Panel area = new Panel();
+            area.Size = new Size(10, 10);
+            area.Location = new Point(x, y);
+            area.BackColor = Color.Blue;
+
+            this.Area.Controls.Add(area);
+        }
+    }
+}
